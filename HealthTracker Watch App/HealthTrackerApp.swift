b0feeadb89003ad8307f9691113a620f3eaceb7c
Tim Terrance <timterrance.tech@gmail.com>
2026-06-01 19:54:56 -0500

    Initial Commit

---
+++ HealthTracker Watch App/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker Watch App
+//
+//  Created by Tim Terrance on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
